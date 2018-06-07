7c6c77af12925fc69f74580819033ac557e2ed11
宇中 <>
2018-06-07 17:05:23 +0800

    68

--- AntCarsRepair/AntCarsRepair/AppDelegate.m
+++ AntCarsRepair/AntCarsRepair/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LRHomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    LRHomeViewController *vc = [[LRHomeViewController alloc] init];
+    vc.title = @"首页";
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ AntCarsRepair/AntCarsRepair/Classes/HomePage/Controller/LRHomeViewController.m
@@ -0,0 +1,24 @@
+//
+//  MYHomeViewController.m
+//  AntCarsRepair
+//
+//  Created by 宇中 on 2018/6/7.
+//  Copyright © 2018年 广州宇中网络科技有限公司. All rights reserved.
+//
+
+#import "LRHomeViewController.h"
+
+@interface LRHomeViewController ()
+
+@end
+
+@implementation LRHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+
+@end
